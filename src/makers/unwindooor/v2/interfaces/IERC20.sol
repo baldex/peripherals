b48107353ac74909b7d899b797b1ef8051411a00
Julo Nizim <julonizim@gmail.com>
2023-09-28 00:12:03 +0700

    Remove all unused stuff

--- src/makers/unwindooor/v2/interfaces/IERC20.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.0;
-
-interface IERC20 {
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function balanceOf(address addy) external view returns (uint256);
-}
